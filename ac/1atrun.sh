393f2e078cdf60c5ec3de107715e964fa4aac204
pgbarletta <pbarletta@gmail.com>
2017-05-23 10:20:01 -0300

    Added new pocket, a more precise definition of the Pocket 1

---
+++ ac/1atrun.sh
@@ -0,0 +1,5 @@
+# /bin/bash
+
+time ~/labo/16/ANA/Debug/ANA 1f41_AC_SL-original.pdb -c 1atcfg.cfg --NDD_input ac_in_ndd --NDD_output 1_at_ac_out_ndd -o 1_at_ac_sal
+
+exit 0
---
+++ bd/1atrun.sh
@@ -0,0 +1,5 @@
+# /bin/bash
+
+time ~/labo/16/ANA/Debug/ANA 1f41_BD_SL-original.pdb -c 1atcfg.cfg --NDD_input bd_in_ndd --NDD_output 1_at_bd_out_ndd -o 1_at_bd_sal
+
+exit 0
